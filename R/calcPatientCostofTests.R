982c0e91ea6915f159a4f4cc593db81f50e89481
Nathan Green <nathan.green@imperial.ac.uk>
2016-01-26 15:10:19 +0000

    `static' version before adding probability sensitivity sampling component of test cost.

--- R/calcPatientCostofTests.R
+++ R/calcPatientCostofTests.R
@@ -1,18 +1,16 @@
 
-#' Sum the cost of all specified treatments for each patient
+#' Sum the Cost of All Specified Treatments for Each Patient
 #'
-#' \code{calcPatientCostofTests} sum the total cost of diagnostic tests in the pathway
+#' \code{calcPatientCostofTests} sums the total cost of diagnostic tests in the pathway.
 #'
 #' If a BAL is taken we include the cost of the proceduce and the additional cost of a culture and smear if used for these.
 #'
-#' @param testdata array of test results
-#' @param x3 should the smear and culture be multiplied by 3? It is standard practice to perform 3 of each with a sputum sample.
+#' @param data IDEA study data set
+#' @param x3 Should the smear and culture be multiplied by 3? It is standard practice to perform 3 of each with a sputum sample.
 #'
 #' @return single value per patient
-#'
 
-
-calcPatientCostofTests <- function(data, x3=TRUE){
+calcPatientCostofTests <- function(data, x3 = TRUE){
 
   freq <- ifelse(x3, 3, 1)
   TBcult.cost <- 22.29
@@ -27,18 +25,28 @@ calcPatientCostofTests <- function(data, x3=TRUE){
                  TSPOT=82, #Local lab cost (IDEA health econ analysis plan)
                  QFN=45, #52 #
                  CXR=28, #NCCCC, Tuberculosis: appendices. London: Royal College of Physicians, 2006;National Collaborating Centre for Chronic Conditions. TB Partial Update: Cost-effectiveness analysis of interferon gamma release assay (IGRA) testing for latent tuberculosis. London: NICE, 2010.
-                 CSF=0, #
-                 BAL=23.24, #St Marys R & D office
+                 CSF=0, #NA for pulmonary TB
+                 BAL=612+23.24, #procedure+test #St Marys R & D office
                  HistBiop=25, #St Marys R & D office
                  NeedleAsp=90.21, #St Marys R & D office
                  PCR=202.45, #St Marys R & D office
                  CT=300, #St Marys R & D office
                  MRI=375, #St Marys R & D office
                  PET=713, #St Marys R & D office
-                 TBcultSmearBAL = TBcult.cost+Smear.cost)
+                 TBcultSmearBAL = TBcult.cost+Smear.cost,
+                 Risk_factors=0)
 
-
-  c(testcosts%*%t(data[,paste("Num_",names(testcosts),sep="")]))
+  ## if there's no test count data
+  if(length(namesNumEachTest)==0){
+    data <- as.matrix(data) #need to do it this way because factor
+    data[data%in%c("POSITIVE", "NEGATIVE")] <- 1
+    data[data!=1 | is.na(data)] <- 0
+    class(data) <- "numeric"
+    testcosts[colnames(data)]%*%t(data)
+  }else{
+    ## cross-product to account for multiple same tests per patient
+    c(testcosts%*%t(data[,paste("Num_",names(testcosts),sep="")]))
+  }
 }
 
 
